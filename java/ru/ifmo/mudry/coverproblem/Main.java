c801e3ea33228986010d06a7ae0228dc42fb671c
nickmydriy <gimbarr21@gmail.com>
2017-02-16 18:28:57 +0300

    implementaion complete, ready for debugging and testing

--- java/ru/ifmo/mudry/coverproblem/Main.java
+++ java/ru/ifmo/mudry/coverproblem/Main.java
@@ -1,7 +1,39 @@
 package ru.ifmo.mudry.coverproblem;
 
+import com.sun.javafx.binding.StringFormatter;
+
+import java.text.DecimalFormat;
+
 /**
  * Created by Nick Mudry on 16.02.2017.
  */
 public class Main {
+
+    static int SIZE = 10;
+
+    public static void main(String[] args) {
+        int[][] test = new int[SIZE][SIZE];
+        double[] testCost = new double[SIZE];
+
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                test[i][j] = Math.random() < 0.5 ? 1 : 0;
+            }
+        }
+
+        for (int i = 0; i < SIZE; i++) {
+            testCost[i] = Math.random() * 10;
+        }
+
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(test[i][j] + " ");
+            }
+            System.out.print("    ");
+            System.out.format("%.2f%n", testCost[i]);
+        }
+
+
+    }
+
 }
